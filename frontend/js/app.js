8df648ee39467632d86b0a58abae6d6a22ecaf2f
KuetTai <kt.xtrik@gmail.com>
2025-08-27 00:40:00 +0800

    Setup GitHub Pages demo with redirect and path fixes

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -48,7 +48,7 @@ class App {
     registerServiceWorker() {
         if ('serviceWorker' in navigator) {
             window.addEventListener('load', () => {
-                navigator.serviceWorker.register('/sw.js')
+                navigator.serviceWorker.register('./sw.js')
                     .then((registration) => {
                         console.log('SW registered: ', registration);
                         
